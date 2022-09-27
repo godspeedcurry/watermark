9c3ddc60dea15e09f1a04dd69799ce21e0792878
godspeedcurry <3160100421@zju.edu.cn>
2022-09-27 18:37:00 +0800

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -1,109 +1,105 @@
 #!/usr/bin/env python3
-import os
-from PIL import ImageFont,ImageDraw,Image
-text = "第五届强网杯专用"
-# 灰色
-color = (0x8f,0x8f,0x8f)
-rot = 10
-def gen_watermark():
-    print('Generate watermark now...')
-    width = 1500
-    height = 1500
-    watermark = Image.new('RGBA',(width,height),(0,0,0,0))
-    font = ImageFont.truetype('fs.ttf',33) 
-    draw = ImageDraw.Draw(watermark)
-    
-    side_width = len(text) * 33 + 50
-    side_height =  80
-    
-    for i in range(width // side_width << 2):
-        for j in range(height // side_height << 2):
-            draw.text((i*side_width,j*side_height),text,font=font,fill=color)
-    
-    watermark = watermark.rotate(rot, expand=1)
-    watermark = watermark.crop((width//2-width//4,height//2-height//4,width//2 + width//4, height//2 + height//4))
-    watermark = watermark.resize((width,height))
-    watermark.save('watermark.png')
-    print('Generate watermark done!')
+from pathlib import Path
+import argparse
+import math
+from typing import *
+from PIL import ImageFont, ImageDraw, Image
 
-def watermark(background,waterimage,output_path):
-    print(f'deal {output_path}, please wait')
-    bg = Image.open(background)
-    wm = Image.open(waterimage)
-    wm = wm.resize(bg.size)
-    # 使水印居中
-    width1,height1 = bg.size
-    width2,height2= wm.size
-    width = (width1-width2) // 2
-    height = (height1-height2) // 2
+SUPPORTED_SUFFIX = ('.jpg', '.png')
+
+
+class InputImage:
+    name: str
+    image: Image.Image
+
+    def __init__(self, name: str, image: Image.Image) -> None:
+        self.name = name
+        self.image = image
+
+
+def gen_watermark(text: str, font: ImageFont.ImageFont, size: Tuple[int, int], rot_degree, color=(0x7f, 0x7f, 0x7f)) -> Image.Image:
+    print('Generating watermark...')
+    
+    width, height = size
+    rot_rad = rot_degree / 180 * math.pi
+    real_width = int(math.ceil(
+        abs(width * math.cos(rot_rad)) + abs(height * math.sin(rot_rad))
+    ))
+    real_height = int(math.ceil(
+        abs(height * math.cos(rot_rad)) + abs(width * math.sin(rot_rad))
+    ))
+
+    watermark = Image.new('RGBA', (real_width, real_height), (255, 0, 0, 0))
+    draw = ImageDraw.Draw(watermark)
+    bbox = font.getbbox(f'    {text}    ')
+    text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    box_width = text_width
+    box_height = text_height * 2
+    for i in range(0, real_width, box_width):
+        for j in range(0, real_height, box_height):
+            draw.text((i, j), text, font=font, fill=color)
+    watermark = watermark.rotate(rot_degree)
+    watermark = watermark.crop(
+        ((real_width - width) // 2, (real_height - height) // 2, (real_width - width) // 2 + width, (real_height - height) // 2 + height))
+    return watermark
+
+
+def watermarkize(bg: Image.Image, wm: Image.Image) -> Image.Image:
+    if wm.size != bg.size:
+        wm = wm.crop((0, 0, bg.width, bg.height))
     # 转换为rgba模式
     if bg.mode != "RGBA":
         bg = bg.convert("RGBA")
     if wm.mode != "RGBA":
         wm = wm.convert("RGBA")
-    # 创建新图层
-    layer = Image.new("RGBA",bg.size,(0,0,0,0))
     # 合并水印文件
-    layer.paste(wm,(width,height))
-    out = Image.composite(layer,bg,layer)
-    # 重命名保存文件
-    out.save(output_path)
-    
-
-def init():
-    print('Initalize workspace now...')
-    os.system('rm -rf ./input')
-    os.system('rm -rf ./output')
+    out = Image.alpha_composite(bg, wm)
+    return out
 
 
-    try:
-        os.mkdir('tmp')
-    except Exception as e:
-        print(e)
-    os.system('mv *.png *.jpg tmp/')
-    
-    try:
-        os.mkdir('input')
-    except Exception as e:
-        print(e)
-    
-    try:
-        os.mkdir('output')
-    except Exception as e:
-        print(e)
-    
-    
-def clean():
-    print("clean workspace now")
-    os.system('rm watermark.png')
-    print("clean done!")
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('text', type=str, help='watermark text')
+    parser.add_argument('-f', '--file', default='.',
+                        type=Path, help='input folder')
+    parser.add_argument('-o', '--output', default='output',
+                        type=Path, help='output folder')
+    parser.add_argument('--rot', default=15,
+                        type=int, help='watermark rotation degree')
+    parser.add_argument('--font-file', default='fs.ttf',
+                        type=Path, help='ttf watermark font')
+    parser.add_argument('--font-size', default=45,
+                        type=int, help='size of watermark font')
+    return parser.parse_args(args)
 
-def main():
-    init()
-    cwd = os.getcwd()
-    # convert format now
-    for tp in os.listdir('./tmp'):
-        if 'DS_Store' in tp:
-            continue
-        orgin_path = os.path.join(cwd, 'tmp', tp)
-        img = Image.open(orgin_path)
-        if orgin_path.endswith('.png'):
-            os.system(f'mv ./tmp/{tp} ./input/{tp}')
-            print(f'{orgin_path} move success')
-        else:
-            input_path = os.path.join(cwd,'input',tp.split('.')[0]+".png")
-            img.save(input_path)
-            print(f'{input_path} convert success')
-    gen_watermark()
+
+def main(args: argparse.Namespace):
+    text: str = args.text
+    file: Path = args.file
+    output: Path = args.output
+    rot: int = args.rot
+    font_file: Path = args.font_file
+    font_size: int = args.font_size
+
+    font = ImageFont.truetype(str(font_file), font_size)
+
+    images: List[InputImage] = []
+    for filename in file.iterdir():
+        if filename.is_file() and filename.name.endswith(SUPPORTED_SUFFIX):
+            images.append(InputImage(filename.name, Image.open(filename)))
     
-    print(f"now we at {cwd}")
+    max_width = max(map(lambda img: img.image.width, images))
+    max_height = max(map(lambda img: img.image.height, images))
+    watermark = gen_watermark(text, font, (max_width, max_height), rot)
     
-    for inp in os.listdir('./input'):
-        input_path = os.path.join(cwd,'input',inp)
-        output_path = os.path.join(cwd,'output',inp)
-        watermark(input_path,'watermark.png', output_path)
-    clean()
+    output.mkdir(parents=True, exist_ok=True)
+    for img in images:
+        print(f'Dealing with {img.name}')
+        with watermarkize(img.image, watermark) as output_image:
+            if img.name.endswith('.jpg'):
+                output_image = output_image.convert('RGB')
+            output_image.save(output / img.name)
+
 
 if __name__ == "__main__":
-    main()
-
+    main(parse_args())
